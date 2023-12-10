4d432a6a12ff48268a620710b3bbcedb0de6fea6
Vitali Paliashchuk <polevit2009@mail.ru>
2023-12-10 13:40:38 +0300

    Revert "Revert "Revert "Revert "Start app, added cases with data""""
    This reverts commit 679450ebb714867eeeb45466952507d52b771f0e.

---
+++ src/components/Costs.js
@@ -0,0 +1,24 @@
+import "./Costs.css"
+
+import Costitem from "./Costitem";
+
+function Costs (props) {
+    return (
+        <div className="costs">
+            <Costitem
+                date={props.costs[0].date}
+                description={props.costs[0].description}
+                amount={props.costs[0].amount}/>
+            <Costitem
+                date={props.costs[1].date}
+                description={props.costs[1].description}
+                amount={props.costs[1].amount}/>
+            <Costitem
+                date={props.costs[2].date}
+                description={props.costs[2].description}
+                amount={props.costs[2].amount}/>
+        </div>
+    )
+}
+
+export default Costs;
